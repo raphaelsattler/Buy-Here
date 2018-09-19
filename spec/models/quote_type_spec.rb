9ef6cb8d89babf4bb34211bf1752f56fba93d768
vinibr3 <vinicius.deoliveira@outlook.com>
2018-09-19 18:36:51 -0300

    Removed atribute order from tests

--- spec/models/quote_type_spec.rb
+++ spec/models/quote_type_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe QuoteType, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_most(255) }
   describe "validations" do
-    subject { QuoteType.create(name: "Type 1", order: "1") }
+    subject { QuoteType.create(name: "Type 1") }
     it { should validate_uniqueness_of(:name) }
   end
 
   it "Defaults active to true" do
-    quote_type = QuoteType.create(name: "Teste", order: "1")
+    quote_type = QuoteType.create(name: "Teste")
     expect(quote_type.active).to be_truthy
   end
 end
