6614d5dacd91716eda29a3eac55f9affa224e1b9
Raphael Sattler <raphaelsattler@hotmail.com>
2018-09-20 17:19:57 -0300

    Add relationship to Group model and Membership model

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Group, type: :model do
 
   it { should validate_uniqueness_of(:name) }
 
+  it { should have_many(:memberships) }
+
   it "the active should be true by default" do
     group = create(:group)
     expect(group.active).to be_truthy
