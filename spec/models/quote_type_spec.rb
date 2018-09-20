09c934a6b9d349629abee1a7cf3d6d1136e4d210
vinibr3 <vinicius.deoliveira@outlook.com>
2018-09-20 18:48:18 -0300

    Used Faker gem in tests

--- spec/models/quote_type_spec.rb
+++ spec/models/quote_type_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe QuoteType, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_most(255) }
   describe "validations" do
-    subject { QuoteType.create(name: "Type 1") }
+    subject { create(:quote_type) }
     it { should validate_uniqueness_of(:name).case_insensitive }
   end
 
   it "Defaults active to true" do
-    quote_type = QuoteType.create(name: "Teste")
+    quote_type = create(:quote_type)
     expect(quote_type.active).to be_truthy
   end
 end
