a73d0db671fbafbe2e071835f57fd76b64b9cdfd
vinibr3 <vinicius.deoliveira@outlook.com>
2018-09-21 15:30:38 -0300

    Created model Product and his tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe Product, type: :model do
+  it { should validate_numericality_of(:code).only_integer }
+  it { should validate_numericality_of(:code).is_greater_than(0) }
+  it { should validate_presence_of(:code) }
+  it { should validate_length_of(:code).is_at_most(255) }
+  it "Uniqueness of code" do
+    create(:product)
+    validate_uniqueness_of(:code)
+  end
+
+  it { should validate_length_of(:name).is_at_most(255) }
+  it { should validate_presence_of(:name) }
+  it "Uniqueness of name" do
+    create(:product)
+    validate_uniqueness_of(:name)
+  end
+
+  it { should validate_presence_of(:description) }
+
+  it "Default active true" do
+    product = create(:product)
+    expect(product.active).to be_truthy
+  end
+
+  it { should validate_numericality_of(:value).is_greater_than_or_equal_to(0) }
+  it { should validate_presence_of(:value) }
+end
