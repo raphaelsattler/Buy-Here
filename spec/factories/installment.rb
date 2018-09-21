bb2d089e6c85fbe2479f49877856e727a2c340db
vinibr3 <vinicius.deoliveira@outlook.com>
2018-09-21 12:02:42 -0300

    Created model PaymentMethod and his tests

--- spec/factories/installment.rb
+++ spec/factories/installment.rb
@@ -1,8 +1,16 @@
 FactoryBot.define do
+  factory :payment_method do
+    name { Faker::Name.name }
+
+    left = Faker::Number.digit.to_i
+    right = Faker::Number.digit.to_i
+    rate { Faker::Number.decimal(left, right) }
+  end
   factory :installment do
     value { Faker::Number.positive.round(2) }
     off { Faker::Number.between(0, 100) }
     due_date { Faker::Date.forward }
     payment_date { Faker::Date.forward }
+    payment_method { PaymentMethod.first || association(:payment_method) }
   end
 end
