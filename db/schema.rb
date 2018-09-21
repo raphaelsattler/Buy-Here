514219875f838a1a70790aa60054f9067a708328
vinibr3 <vinicius.deoliveira@outlook.com>
2018-09-21 15:30:38 -0300

    Created model Product and his tests

--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,9 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2018_09_23_172348) do
 
+ActiveRecord::Schema.define(version: 2018_09_23_172348) do
+  
   create_table "installments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.decimal "off", precision: 4, scale: 2, default: "0.0"
     t.decimal "value", precision: 12, scale: 2, null: false
@@ -30,6 +31,18 @@ ActiveRecord::Schema.define(version: 2018_09_23_172348) do
     t.decimal "rate", precision: 10, default: "0", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "code", null: false
+    t.string "name", null: false
+    t.text "description"
+    t.boolean "active", default: true, null: false
+    t.decimal "value", precision: 12, scale: 2, default: "0.0", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_products_on_code"
+    t.index ["name"], name: "index_products_on_name"
   end
 
   create_table "quote_statuses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
