fc87e45c7e53328ae9e13e5d31b8ae83c2558c59
raphaelsattler <raphaelsattler@hotmail.com>
2018-09-23 17:58:39 -0300

    Fix syntax error

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Membership, type: :model do
   it { should belong_to(:user) }
-  
+
   it { should belong_to(:group) }
 
   it "the active should be true by default" do
