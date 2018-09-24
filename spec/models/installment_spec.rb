7aff32db4f4cabbcc82d80500a77a86cef3018b5
vinibr3 <vinicius.deoliveira@outlook.com>
2018-09-24 19:05:41 -0300

    Sintax fixbug

--- spec/models/installment_spec.rb
+++ spec/models/installment_spec.rb
@@ -14,11 +14,13 @@ RSpec.describe Installment, type: :model do
 
   it { should validate_numericality_of(:total_value).is_greater_than_or_equal_to(0) }
   it { should validate_presence_of(:total_value) }
-  context do
+
+  context "when validate calculates total value" do
     let(:installment) { create(:installment) }
     let(:value) { installment.value }
     let(:off) { installment.off }
     let(:total_value) { value - (value * (off / 100)) }
+
     it "calculate total value" do
       expect(installment.total_value.round(2)).to eq(total_value.round(2))
     end
