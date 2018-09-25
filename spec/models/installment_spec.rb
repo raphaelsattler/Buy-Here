98e453d088e2712edce0f562e8006fd26aa6687e
vinibr3 <vinicius.deoliveira@outlook.com>
2018-09-25 09:42:12 -0300

    Sintax fixbug

--- spec/models/installment_spec.rb
+++ spec/models/installment_spec.rb
@@ -4,14 +4,10 @@ RSpec.describe Installment, type: :model do
   it { is_expected.to validate_numericality_of(:off).is_greater_than_or_equal_to(0) }
   it { is_expected.to validate_numericality_of(:off).is_less_than_or_equal_to(100) }
   it { is_expected.to validate_presence_of(:off) }
-
   it { is_expected.to validate_numericality_of(:value).is_greater_than_or_equal_to(0) }
   it { is_expected.to validate_presence_of(:value) }
-
   it { is_expected.to validate_presence_of(:due_date) }
-
   it { is_expected.to validate_presence_of(:payment_date) }
-
   it { is_expected.to validate_numericality_of(:total_value).is_greater_than_or_equal_to(0) }
   it { is_expected.to validate_presence_of(:total_value) }
 
