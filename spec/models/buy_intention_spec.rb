3111dab8d82fa8aebac14d6b4993f190e128a5ac
Raphael Sattler <raphaelsattler@hotmail.com>
2018-09-25 16:00:00 -0300

    Removed empty spaces

--- spec/models/buy_intention_spec.rb
+++ spec/models/buy_intention_spec.rb
@@ -2,9 +2,7 @@ require "rails_helper"
 
 RSpec.describe BuyIntention, type: :model do
   it {  is_expected.to have_many(:people) }
-
   it {  is_expected.to validate_presence_of(:name) }
-
   it {  is_expected.to validate_uniqueness_of(:name) }
 
   it "the active should be true by default" do
