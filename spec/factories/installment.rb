731ef57de123dce262ac80d543571a77b8da3b96
vinibr3 <vinicius.deoliveira@outlook.com>
2018-09-25 16:00:45 -0300

    Removed factories.rb

---
+++ spec/factories/installment.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :installment do
+    digit = Faker::Number.digit
+    value = Faker::Number.decimal(digit.to_i, 2)
+
+    value { value }
+    off { Faker::Number.between(0, 100) }
+    due_date { Faker::Date.forward }
+    payment_date { Faker::Date.forward }
+    payment_method { association(:payment_method) }
+  end
+end
