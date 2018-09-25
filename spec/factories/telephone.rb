00165c3ca80faa1abdcbce27b73484466baf2d01
Raphael Sattler <raphaelsattler@hotmail.com>
2018-09-25 16:19:38 -0300

    Improve factories feature

---
+++ spec/factories/telephone.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :telephone do
+    person { association(:person) }
+    telephone_type { association(:telephone_type) }
+    ddi_number { Faker::Number.between(1, 999) }
+    ddd_number { Faker::Number.between(1, 99) }
+    telephone_number { Faker::Number.number(9) }
+  end
+end
