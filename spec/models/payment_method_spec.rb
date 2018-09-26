e3b465f139c90563efde8adc3bf7d939b2131c99
vinibr3 <vinicius.deoliveira@outlook.com>
2018-09-26 18:23:34 -0300

    Update payment_method_spec.rb

--- spec/models/payment_method_spec.rb
+++ spec/models/payment_method_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe PaymentMethod, type: :model do
+  let(:payment_method) { create(:payment_method) }
+  
   it { is_expected.to have_many(:installments) }
   it { is_expected.to validate_length_of(:name).is_at_most(255) }
   it { is_expected.to validate_presence_of(:name) }
@@ -8,17 +10,14 @@ RSpec.describe PaymentMethod, type: :model do
   it { is_expected.to validate_presence_of(:rate) }
 
   describe "validations" do
-    subject { create(:payment_method) }
     it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   end
 
   it "Default to due false" do
-    payment_method = create(:payment_method)
     expect(payment_method.due).to_not be_truthy
   end
 
   it "Default to active true" do
-    payment_method = create(:payment_method)
     expect(payment_method.active).to be_truthy
   end
 end
