e41c6be07626d13acb722708a514912f587dd4af
vinibr3 <vinicius.deoliveira@outlook.com>
2018-09-26 14:36:31 -0300

    Update product_spec.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Product, type: :model do
   it { is_expected.to validate_length_of(:code).is_at_most(255) }
   it { is_expected.to validate_length_of(:name).is_at_most(255) }
   it { is_expected.to validate_presence_of(:name) }
-  it { should validate_presence_of(:description) }
+  it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_numericality_of(:value).is_greater_than_or_equal_to(0) }
   it { is_expected.to validate_presence_of(:value) }
 
