a4896fe6d8090fb0e6f38821f462c9332656b799
Raphael Sattler <raphaelsattler@hotmail.com>
2018-10-02 18:15:33 -0300

    Implement style and tests

--- spec/features/person_list_spec.rb
+++ spec/features/person_list_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
 RSpec.feature "People List" do
-  let(:person1) { create(:person) }
-  let(:person2) { create(:person) }
-  let(:person3) { create(:person) }
+  let(:user) { create(:user) }
+  let(:person1) { create(:person, user: user) }
+  let(:person2) { create(:person, user: user) }
+  let(:person3) { create(:person, user: user) }
+
   scenario "when the root page is load should show a list of persons" do
     person1
     person2
