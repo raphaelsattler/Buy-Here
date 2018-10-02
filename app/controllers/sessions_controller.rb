87da2331a43ba5e81d975f291dad6f33477d3f62
vinibr3 <vinicius.deoliveira@outlook.com>
2018-10-02 16:15:55 -0300

    Features file to singular

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: session_params[:email])
-    if user && user.authenticate(session_params[:password])
+    user = User.find_by(email: valid_params[:email])
+    if user && user.authenticate(valid_params[:password])
       sign_in user
       flash[:success] = t("activerecord.session.form.flash.login_success")
       redirect_to user
