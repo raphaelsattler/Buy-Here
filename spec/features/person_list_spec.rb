7bae54585fa63953d7caf17ed4269247efa8c1a6
Raphael Sattler <raphaelsattler@hotmail.com>
2018-10-03 10:28:17 -0300

    Fix intermittent test

--- spec/features/person_list_spec.rb
+++ spec/features/person_list_spec.rb
@@ -2,9 +2,11 @@ require "rails_helper"
 
 RSpec.feature "People List" do
   let(:user) { create(:user) }
-  let(:person1) { create(:person, user: user) }
-  let(:person2) { create(:person, user: user) }
-  let(:person3) { create(:person, user: user) }
+  let(:profile) { create(:profile) }
+  let(:buy_intention) { create(:buy_intention) }
+  let(:person1) { create(:person, user: user, profile: profile, buy_intention: buy_intention) }
+  let(:person2) { create(:person, user: user, profile: profile, buy_intention: buy_intention) }
+  let(:person3) { create(:person, user: user, profile: profile, buy_intention: buy_intention) }
 
   scenario "when the root page is load should show a list of persons" do
     person1
