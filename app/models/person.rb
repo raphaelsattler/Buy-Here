55c715452b3ccd86a7d581435731b70d6fa59cc5
Raphael Sattler <raphaelsattler@hotmail.com>
2018-10-04 15:21:58 -0300

    Create new person feature

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,10 +2,10 @@ class Person < ApplicationRecord
   validates :name, presence: { if: -> { social_name.blank? } }
   validates :social_name, presence: { if: -> { name.blank? } }
   validates :email, uniqueness: true, presence: true, email_address: true
-  validates :cpf, cpf: { if: -> { cnpj.blank? } }, uniqueness:  { if: -> { !cpf.blank? } }
-  validates :cnpj, cnpj: { if: -> { cpf.blank? } }, uniqueness: { if: -> { !cnpj.blank? } }
-  validates :rg, uniqueness: true
-  validates :uf_expediter_rg, length: { is: 2 }
+  validates :cpf, cpf: { if: -> { cnpj.blank? } }, uniqueness:  { unless: -> { cpf.blank? } }
+  validates :cnpj, cnpj: { if: -> { cpf.blank? } }, uniqueness: { unless: -> { cnpj.blank? } }
+  validates :rg, uniqueness: { unless: -> { rg.blank? } }
+  validates :uf_expediter_rg, length: { is: 2, unless: ->  { uf_expediter_rg.blank? } }
   validates :active, inclusion: { in: [true, false] }
 
   has_many :telephones, dependent: :destroy
